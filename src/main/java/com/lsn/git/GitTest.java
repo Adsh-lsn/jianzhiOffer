9cccacdd12a1b78a73bd77793f5a3c249f337a0e
lsn <lsn@qq.com>
2021-09-06 17:10:53 +0800

    firstCommit

---
+++ src/main/java/com/lsn/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.lsn.git;
+
+/**
+ * @author LiuShengnan
+ * @date 2021/9/6 17:08
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
